8bda89644456d8211536c29b2221c92399fc0ed0
balumaqsud <ollobergan008@mail.ru>
2025-02-22 22:14:36 +0900

    train callback

--- train.js
+++ train.js
@@ -1,1 +1,33 @@
-console.log("this is TRAIN JS");
+const adviceList = [
+  "Keep learning every single day", //0-20
+  "Stay focused on your goals", //20-30
+  "Take breaks to avoid burnout", //30-40
+  "Practice makes everything much better", //40-50
+  "Ask questions and seek help", //50-60
+  "Never fear making small mistakes", //60 -
+];
+
+//def
+function giveAdvice(age, callback) {
+  if (typeof age !== "number") callback("insert number", null);
+  else if (age <= 20) callback(null, adviceList[0]);
+  else if (age > 20 && age <= 30) callback(null, adviceList[1]);
+  else if (age > 30 && age <= 40) callback(null, adviceList[2]);
+  else if (age > 40 && age <= 50) callback(null, adviceList[3]);
+  else if (age > 50 && age <= 60) callback(null, adviceList[4]);
+  else {
+    setTimeout(function () {
+      callback(null, adviceList[5]);
+    }, 5000);
+  }
+}
+
+//call
+console.log("----------------1");
+giveAdvice(65, (err, data) => {
+  if (err) console.log("error:", err);
+  else {
+    console.log("answer:", data);
+  }
+});
+console.log("----------------2");
