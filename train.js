e076ca78edd30b3fe21421a125046b7c8c1ab5a7
balumaqsud <ollobergan008@mail.ru>
2025-02-25 15:13:04 +0900

    testing before deleting author's docs

--- train.js
+++ train.js
@@ -1,28 +1,28 @@
-//A-TASK:
-// example countLetter("e", "engineer");
+// //A-TASK:
+// // example countLetter("e", "engineer");
 
-let count = 0;
-const letterCounter = (target, string) => {
-  for (let i = 0; i < string.length; i++) {
-    if (target === string[i]) {
-      count++;
-    }
-  }
-  console.log("count:", count);
-  return count;
-};
+// let count = 0;
+// const letterCounter = (target, string) => {
+//   for (let i = 0; i < string.length; i++) {
+//     if (target === string[i]) {
+//       count++;
+//     }
+//   }
+//   console.log("count:", count);
+//   return count;
+// };
 
-letterCounter("l", "Ollobergan Abdulla Maksud Ugli");
+// letterCounter("l", "Ollobergan Abdulla Maksud Ugli");
 
-//////////////////////////////////
-const adviceList = [
-  "Keep learning every single day", //0-20
-  "Stay focused on your goals", //20-30
-  "Take breaks to avoid burnout", //30-40
-  "Practice makes everything much better", //40-50
-  "Ask questions and seek help", //50-60
-  "Never fear making small mistakes", //60 -
-];
+// //////////////////////////////////
+// const adviceList = [
+//   "Keep learning every single day", //0-20
+//   "Stay focused on your goals", //20-30
+//   "Take breaks to avoid burnout", //30-40
+//   "Practice makes everything much better", //40-50
+//   "Ask questions and seek help", //50-60
+//   "Never fear making small mistakes", //60 -
+// ];
 
 // function giveAdvice(age, callback) {
 //   if (typeof age !== "number") callback("insert number", null);
@@ -39,21 +39,17 @@ const adviceList = [
 // }
 
 // console.log("----------------1");
-// giveAdvice(65, (err, data) => {
+// giveAdvice(45, (err, data) => {
 //   if (err) console.log("error:", err);
 //   else {
 //     console.log("answer:", data);
 //   }
 // });
-// console.log("----------------2");
+// // console.log("----------------2");
 
 // async function giveAsyncAdvice(age) {
 //   if (typeof age !== "number") throw new Error("insert number");
 //   else if (age <= 20) return adviceList[0];
-//   else if (age > 20 && age <= 30) return adviceList[1];
-//   else if (age > 30 && age <= 40) return adviceList[2];
-//   else if (age > 40 && age <= 50) return adviceList[3];
-//   else if (age > 50 && age <= 60) return adviceList[4];
 //   else {
 //     return new Promise((resolve, reject) => {
 //       setTimeout(() => {
@@ -64,26 +60,19 @@ const adviceList = [
 // }
 
 // //////call with then() and catch()
-
 // console.log("----------------1");
 // giveAsyncAdvice(25)
 //   .then((data) => console.log("async answer:", data))
 //   .catch((err) => err);
 // console.log("----------------2");
 
-// /////// with await to escape callback hell
-
+// // /////// with await to escape callback hell
 // async function giveAnswer() {
 //   let answer = await giveAsyncAdvice(20);
 //   console.log("with await: ", answer);
 //   answer = await giveAsyncAdvice(90);
 //   console.log("with await: ", answer);
 //   answer = await giveAsyncAdvice(40);
-//   console.log("with await: ", answer);
-//   answer = await giveAsyncAdvice(50);
-//   console.log("with await: ", answer);
-//   answer = await giveAsyncAdvice(60);
-//   console.log("with await: ", answer);
 // }
 // giveAnswer();
 
