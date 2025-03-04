e660e72824105be1fc81a2d4a7c7aa8ea4f97060
balumaqsud <ollobergan008@mail.ru>
2025-03-04 21:59:08 +0900

    d-task

--- train.js
+++ train.js
@@ -1,3 +1,45 @@
+// Shunday class tuzing tuzing nomi Shop, va uni constructoriga
+//  3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin,
+//   biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+// MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq()
+// return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud!
+// shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq()
+// return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+
+const moment = require("moment");
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.prodcuts = { non, lagmon, cola };
+    this.now = moment().format("hh:mm a");
+  }
+  //methods
+  sell(product_name, product_count) {
+    if (product_count > this.prodcuts[product_name]) {
+      console.log(`we do not have ${product_count} of ${product_name}`);
+    } else {
+      this.prodcuts[product_name] -= product_count;
+      console.log(`At ${this.now} we sell ${product_count} of ${product_name}`);
+    }
+  }
+  recieve(product_name, product_count) {
+    this.prodcuts[product_name] += product_count;
+    console.log(
+      `At ${this.now} we recived ${product_count} of ${product_name}`
+    );
+  }
+  remainder() {
+    console.log(
+      `At ${this.now}, We have ${this.prodcuts["non"]}ta non, ${this.prodcuts["lagmon"]} lagmon , ${this.prodcuts["cola"]} cola!`
+    );
+  }
+}
+
+const shop = new Shop(10, 10, 10);
+shop.recieve("lagmon", 10);
+shop.remainder();
+shop.sell("lagmon", 19);
+shop.remainder();
+
 // //C - task;
 // const checkTwoWords = (str1, str2) => {
 //   let count = 0;
